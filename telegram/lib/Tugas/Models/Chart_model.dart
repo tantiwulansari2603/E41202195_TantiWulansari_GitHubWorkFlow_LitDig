9c297c7254306e784359d55bb80dbc2724cd3541
Tanti Wulansari <75100232+tantiwulansari2603@users.noreply.github.com>
2022-05-24 16:46:04 +0100

    Add folder lib dan file lainnya

---
+++ telegram/lib/Tugas/Models/Chart_model.dart
@@ -0,0 +1,73 @@
+import 'dart:ui';
+
+class ChartModel {
+  final String? name;
+  final String? message;
+  final String? time;
+  final String? profileUrl;
+
+  ChartModel({this.name, this.message, this.time, this.profileUrl});
+}
+
+final List<ChartModel> items = [
+  ChartModel(
+      name: 'Jefri Nicol',
+      message: 'Hello Taan',
+      time: '08.00',
+      profileUrl:
+          'https://obs.line-scdn.net/0h9jY97ARhZkVkQE_muf8ZEl4WZSpXLHVGAHY3WzQuOHEeIiRGWXIgK0dGbSJMdiEbDSUtI0hCfXQccSBEXnMg/w1200'),
+  ChartModel(
+      name: 'Iqbal Ramadhan',
+      message: 'Lagi apa??',
+      time: '9 march',
+      profileUrl:
+          'https://akcdn.detik.net.id/community/media/visual/2020/02/13/050ffc54-66be-464c-81c8-12c861c1864e_43.jpeg?w=700&q=90'),
+  ChartModel(
+      name: 'Angga Aldi Yunanda',
+      message: 'Udah makan?',
+      time: '16 march',
+      profileUrl:
+          'https://assets.pikiran-rakyat.com/crop/267x147:736x497/x/photo/2021/08/10/1143241431.png'),
+  ChartModel(
+      name: 'Fadil Jaidi',
+      message: 'Gak kuliah?',
+      time: '12.21',
+      profileUrl:
+          'https://akcdn.detik.net.id/community/media/visual/2021/05/17/fadil-jaidi_169.jpeg?w=700&q=90'),
+  ChartModel(
+      name: 'Yislam Jaidi',
+      message: 'Gak istirahat?',
+      time: '21.00',
+      profileUrl:
+          'https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/12/22/2020498917.jpeg'),
+  ChartModel(
+      name: 'Jerome Polin',
+      message: 'Ini sudah di Jepang aku Tan',
+      time: '25 january',
+      profileUrl:
+          'https://akcdn.detik.net.id/community/media/visual/2020/08/14/jerome-polin-14_43.jpeg?w=700&q=90'),
+  ChartModel(
+      name: 'Jourdy Pranata',
+      message: 'Hai Tanti',
+      time: '19.00',
+      profileUrl:
+          'https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/10/25/105907220.jpeg'),
+  ChartModel(
+      name: 'Al-Ghazali',
+      message: 'Apa kabar?',
+      time: '12.21',
+      profileUrl:
+          'https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/07/26/1959788038.jpg'),
+  ChartModel(
+      name: 'Dila Jaidi',
+      message: 'Apa kabar?',
+      time: '12.21',
+      profileUrl:
+          'https://assets.pikiran-rakyat.com/crop/4x12:433x512/x/photo/2021/12/22/3669768693.png'),
+  ChartModel(
+      name: 'Adele',
+      message: 'Apa kabar?',
+      time: '12.21',
+      profileUrl:
+          'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/rockcms/2022-02/adele-main-jc-220215-7430fb.jpg'),
+];
